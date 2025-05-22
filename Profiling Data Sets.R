a75ef0f5538b894734d89fe9b92b07fbfbc91733
BarryDeCicco <bdecicco2001@yahoo.com>
2025-05-22 19:07:24 +0000

    Moved data set prperation to first_synthesis.r

--- Profiling Data Sets.R
+++ Profiling Data Sets.R
@@ -1,14 +1,8 @@
 # Profiling Data Sets.r
 
 
-mysyn_dataframe <- as.data.frame(mysyn$syn)
-mydata_dataframe <- as.data.frame(mydata)
-is.data.frame(mysyn_dataframe)
-is.data.frame(mydata_dataframe)
 
-# save data sets for later reuse:
-saveRDS(mysyn_dataframe, file = "mysyn_dataframe.rds")
-saveRDS(mydata_dataframe, file = "mydata_dataframe.rds")
+
 
 
 # Comparedf function from the package 'Arsenal':
