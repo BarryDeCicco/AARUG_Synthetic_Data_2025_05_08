25daf9da3754e4c6425ee2a041b4e46fabd74cfb
BarryDeCicco <bdecicco2001@yahoo.com>
2025-05-22 18:48:22 +0000

    Pretty much a complete update; most rows pulled from the synthesis file.

--- Profiling Data Sets.R
+++ Profiling Data Sets.R
@@ -1,13 +1,28 @@
 # Profiling Data Sets.r
 
 
-install.packages("arsenal")
+mysyn_dataframe <- as.data.frame(mysyn$syn)
+mydata_dataframe <- as.data.frame(mydata)
+is.data.frame(mysyn_dataframe)
+is.data.frame(mydata_dataframe)
+
+# save data sets for later reuse:
+saveRDS(mysyn_dataframe, file = "mysyn_dataframe.rds")
+saveRDS(mydata_dataframe, file = "mydata_dataframe.rds")
+
+
+# Comparedf function from the package 'Arsenal':
+
+
+# https://www.rdocumentation.org/packages/compareDF/versions/2.3.5 and
+# https://cran.r-project.org/web/packages/arsenal/vignettes/comparedf.html
+
+# install.packages("arsenal")
 
 library(arsenal)
 
-summary(comparedf(mysyn, mydata))
+summary(comparedf(mysyn_dataframe, mydata_dataframe))
 
-typeof(mysyn)
-typeof(mydata)
+
 
 
